cc15558967bedff2abeec40597b9c41aa872b1ac
EGOIST <0x142857@gmail.com>
2024-10-14 18:10:54 +0800

    update

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import { BrowserWindow } from "electron"
+
+export const makeKeyWindow: (window: BrowserWindow) => void
+export const makePanel: (window: BrowserWindow) => void
+export const makeWindow: (window: BrowserWindow) => void
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var isMac = process.platform === 'darwin';
-var NativeExtension = isMac ? require('bindings')('NativeExtension') : undefined;
+var isMac = process.platform === "darwin"
+var NativeExtension = isMac ? require("bindings")("NativeExtension") : undefined
 
-module.exports = {
-  makeKeyWindow: function(window) {
-    if (!isMac) return;
-    return NativeExtension.MakeKeyWindow(window.getNativeWindowHandle());
-  },
-  makePanel: function(window) {
-    if (!isMac) return;
-    return NativeExtension.MakePanel(window.getNativeWindowHandle());
-  },
-  makeWindow: function(window) {
-    if (!isMac) return;
-    return NativeExtension.MakeWindow(window.getNativeWindowHandle());
-  }
+exports.makeKeyWindow = function (window) {
+  if (!isMac) return
+  return NativeExtension.MakeKeyWindow(window.getNativeWindowHandle())
+}
+
+exports.makePanel = function (window) {
+  if (!isMac) return
+  return NativeExtension.MakePanel(window.getNativeWindowHandle())
+}
+
+exports.makeWindow = function (window) {
+  if (!isMac) return
+  return NativeExtension.MakeWindow(window.getNativeWindowHandle())
 }
